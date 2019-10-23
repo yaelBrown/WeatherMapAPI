79553817c2cc353f5da9a0a1b280d84f24b54e36
yaelBrown <yaelrbrown@gmail.com>
2019-10-23 15:47:22 -0500

    Moved project from codeup-web-exercises into own repo.

---
+++ index_files/keys.js
@@ -0,0 +1,11 @@
+// Map Box
+var mapBoxToken = 'pk.eyJ1IjoieWFlbGJyb3duIiwiYSI6ImNqemVidGhsMjAwc2szZW8wbW51bTFwN2gifQ.Xps4U82N0BsZxh79qcYdRw';
+console.log("mapbox key is loaded");
+
+// Dark Sky
+var darkSkyKey = "3a3c1b32f4117f09303b866e6843c330";
+console.log("DarkSky API key loaded.");
+
+// Gh 
+let githubToken = "731b4f093335e5cb2d5d8f45c2652d51a86e5b26";
+console.log("gh key loaded.");
